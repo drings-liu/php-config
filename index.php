aebf3aacd968663bc630a0f75ab6ea0619db9a1f
drings-liu <drings@126.com>
2018-08-21 18:29:23 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+include('config/db_config.php');
+
+
+
+echo " 连接数据库地址是：".$db_config['db_user_host'];
+
+echo " 连接数据库地址是：".$db_config['db_user_name'];
+
+echo " 连接数据库地址是：".$db_config['db_user_pswd'];
+
+
+var_dump($db_config);

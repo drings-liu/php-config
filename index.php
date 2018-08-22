53292aa785c20adce7c42ec5ef143445bf9afbaa
kevin.liu <kevin.liu@idreamsky.com>
2018-08-22 17:39:43 +0800

    增加配置文件

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
 
 include('config/db_config.php');
-
-
-
 echo " 连接数据库地址是：".$db_config['db_user_host'];
-
+echo "\n";
 echo " 连接数据库地址是：".$db_config['db_user_name'];
-
+echo "\n";
 echo " 连接数据库地址是：".$db_config['db_user_pswd'];
-
-echo "hello configmap";
-
+echo "\n";
+echo "hello configmap \n";
 var_dump($db_config);

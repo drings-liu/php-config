917167c1bbb23edbc2ffe690e6e7ed1f1f90fdc6
kevin.liu <kevin.liu@idreamsky.com>
2018-08-27 16:04:44 +0800

    修改php文件

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ echo "连接数据库地址是：".$db_config['db_user_host'];
 echo "连接数据库地址是：".$db_config['db_user_name'];
 
 echo "连接数据库地址是：".$db_config['db_user_pswd'];
-echo "restart nginx test";
-
+echo "restart nginx test <br/>";
 echo "hello configmap <br/>";
 var_dump($db_config);
